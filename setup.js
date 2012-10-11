5ef1c70f8ff6458329a6ada072f10a525e175ada
Paritosh Shah <paritosh@pocketgems.com>
2012-10-11 15:51:57 -0700

    v1 of the extension

---
+++ setup.js
@@ -0,0 +1,15 @@
+      // Called when the url of a tab changes.
+      function checkForValidUrl(tabId, changeInfo, tab) {
+        // If the letter 'g' is found in the tab's URL...
+        if (tab.url.indexOf('https://www.waiter.com') > -1) {
+          // ... show the page action.
+          chrome.pageAction.show(tabId);
+        }
+      };
+
+      // Listen for any changes to the URL of any tab.
+      chrome.tabs.onUpdated.addListener(checkForValidUrl);			
+			chrome.pageAction.onClicked.addListener(function(tab) {
+				chrome.tabs.executeScript(null, { file: "veg.waiter.user.js" });
+			});
+ 
---
+++ veg.waiter.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name Veg Waiter.com
+// @description Highlight vegetarian options on Waiter.com
+// @match http://waiter.com/*
+// @match https://waiter.com/*
+// @match https://*.waiter.com/*
+// ==/UserScript==
+
+var blacklist = new RegExp("Bacon|Beef|BLT|Burger|Clam|Calamari|Catfish|Chicken|Crab|Duck|Eel|Fish|Frog|Ham|Lamb|Meat|Mussels|Octopus|Pastrami|Pig|Pork|Prawn|prosciutto|salami|Salmon|sausage|Seafood|Shrimp|Sirloin|Steak|Rib|Trout|Tuna|Turkey|Wings", "i");
+var whitelist = new RegExp("or veg|or tofu|or mushroom|vegg", "i");
+var items = document.getElementsByClassName('menu_item_container');
+for (i in items) {
+  try {
+    if (!items[i].innerText.match(blacklist) || items[i].innerText.match(whitelist)) {
+      items[i].style.backgroundColor = 'lightgreen';
+    }
+  } catch(e) {};
+}
+
