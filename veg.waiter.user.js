c3da93811a6fdd2e5bb658a0ed7d094740816842
Mohit <mohit@pocketgems.com>
2013-02-14 02:09:04 -0800

    Add check for items containing egg

--- veg.waiter.user.js
+++ veg.waiter.user.js
@@ -7,13 +7,19 @@
 // ==/UserScript==
 
 var blacklist = new RegExp("Bacon|Beef|BLT|Burger|Clam|Calamari|Catfish|Chicken|Crab|Duck|Eel|Fish|Frog|Ham|Lamb|Meat|Mussels|Octopus|Pastrami|Pig|Pork|Prawn|prosciutto|salami|Salmon|sausage|Seafood|Shrimp|Sirloin|Steak|Rib|Trout|Tuna|Turkey|Wings|Pepperoni|Steak", "i");
+var egglist = new RegExp("Egg |eggs", "i")
 var whitelist = new RegExp("or veg|or tofu|or mushroom|vegg", "i");
 var items = document.getElementsByClassName('menu_item_container');
 for (i in items) {
-  try {
-    if (!items[i].innerText.match(blacklist) || items[i].innerText.match(whitelist)) {
-      items[i].style.backgroundColor = 'lightgreen';
-    }
-  } catch(e) {};
+    try {
+        var isVeg = false;
+        if (!items[i].innerText.match(blacklist) || items[i].innerText.match(whitelist)) {
+            items[i].style.backgroundColor = 'lightgreen';
+            isVeg = true; 
+        }
+        if (isVeg && items[i].innerText.match(egglist)) {
+            items[i].style.backgroundColor = 'yellow';
+        } 
+    } catch(e) {};
 }
 
